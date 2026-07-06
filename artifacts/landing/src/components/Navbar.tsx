0df9bc8d53974a7ae0b4c129f96d8d9ddce9e005
ElonInvestmentCo <managementelonmusk16@gmail.com>
2026-07-06 23:24:47 +0000

    FIX

--- artifacts/landing/src/components/Navbar.tsx
+++ artifacts/landing/src/components/Navbar.tsx
@@ -29,12 +29,12 @@ export function Navbar() {
       }`}
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16 lg:h-20">
+        <div className="flex items-center justify-between h-20 lg:h-24">
           <Link to="/" className="flex items-center">
             <img
               src="/payvora-wordmark.svg"
               alt="PAYVORA"
-              style={{ height: "32px", width: "auto", filter: "brightness(0) invert(1)" }}
+              style={{ height: "80px", width: "auto", filter: "brightness(0) invert(1)" }}
             />
           </Link>
 
